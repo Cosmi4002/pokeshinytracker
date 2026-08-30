11628934644f5be8815c6b8c0ba6fd115f372a43
Cosmi4002 <ugo4002@gmail.com>
2026-08-30 21:21:53 +0200

    Update Gen 5 optimized sprite tests

--- src/lib/game-sprites.test.ts
+++ src/lib/game-sprites.test.ts
@@ -5,16 +5,16 @@ describe('game-specific shiny sprites', () => {
   it.each([
     ['heartgold', '/img/game-sprites/hgss/Spr_4h_001_s.png'],
     ['soulsilver', '/img/game-sprites/hgss/Spr_4h_001_s.png'],
-    ['black', '/img/game-sprites/bw/Spr_5b_001_s.png'],
-    ['white', '/img/game-sprites/bw/Spr_5b_001_s.png'],
-    ['black2', '/img/game-sprites/bw2/Spr_5b_001_s.png'],
-    ['white2', '/img/game-sprites/bw2/Spr_5b_001_s.png'],
+    ['black', '/img/game-sprites/bw/Spr_5b_001_s.webp'],
+    ['white', '/img/game-sprites/bw/Spr_5b_001_s.webp'],
+    ['black2', '/img/game-sprites/bw/Spr_5b_001_s.webp'],
+    ['white2', '/img/game-sprites/bw/Spr_5b_001_s.webp'],
   ])('resolves Bulbasaur for %s', (gameId, expected) => {
     expect(getGameSpecificShinySpriteUrl(1, gameId, { name: 'bulbasaur' })).toBe(expected);
   });
 
   it('uses a Black 2 / White 2 override when one exists', () => {
-    expect(getGameSpecificShinySpriteUrl(17, 'black2', { name: 'pidgeotto' }))
-      .toBe('/img/game-sprites/bw2/Spr_5b2_017_s.png');
+    expect(getGameSpecificShinySpriteUrl(495, 'black2', { name: 'snivy' }))
+      .toBe('/img/game-sprites/bw2/Spr_5b2_495_s.webp');
   });
 });
